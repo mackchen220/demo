7ced6faa4556e2cc1fa0807db3dbdd045b530059
陈宇 <>
2021-02-06 15:30:01 +0800

    无

--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/user/mapper/TalentInfoModelMapper.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/user/mapper/TalentInfoModelMapper.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface TalentInfoModelMapper {
+
     int deleteByPrimaryKey(String id);
 
     int insert(TalentInfoModel record);
--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/user/service/TalentInfoModelService.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/user/service/TalentInfoModelService.java
@@ -1,11 +1,8 @@
 package org.jeecg.modules.user.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.jeecg.modules.Community.model.CommunityModel;
 import org.jeecg.modules.user.model.TalentInfoModel;
 import org.jeecg.modules.user.model.vo.TalentInfoVo;
-
-import java.util.List;
 
 public interface TalentInfoModelService{
 
