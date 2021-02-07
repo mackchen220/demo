32c0e8f28834b17c4d1eed6e29a002302d23218d
陈宇 <>
2021-02-07 21:24:14 +0800

    搜索医院，医院列表

---
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/user/model/vo/UserModelVo.java
@@ -0,0 +1,24 @@
+package org.jeecg.modules.user.model.vo;
+
+import lombok.Data;
+
+@Data
+public class UserModelVo {
+    /**
+    * 主键
+    */
+    private String Id;
+
+
+    /**
+    * 昵称
+    */
+    private String nickName;
+
+    /**
+    * 头像
+    */
+    private String headImage;
+
+
+}
